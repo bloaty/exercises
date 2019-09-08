ec610ddfb1403f4603badb43c59fffafea54a35c
Alex Smaliy <alex.smaliy@gmail.com>
2019-09-08 00:47:36 -0700

    simple problem

---
+++ src/compprog3e/onlinejudge/OnlineJudge11364Parking.java
@@ -0,0 +1,34 @@
+package compprog3e.onlinejudge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Small enough to do a linear search.
+public class OnlineJudge11364Parking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numCases = scanner.nextInt();
+        for (int i = 0; i < numCases; i++) {
+            int numLocations = scanner.nextInt();
+            int[] positions = new int[numLocations];
+            for (int j = 0; j < numLocations; j++) {
+                positions[j] = scanner.nextInt();
+            }
+            int bestSum = Integer.MAX_VALUE;
+            int max = Arrays.stream(positions).max().getAsInt();
+            int min = Arrays.stream(positions).min().getAsInt();
+            for (int k = 0; k < 100; k++) {
+                int sum;
+                if (k < min) {
+                    sum = 2 * (max - k);
+                } else if (k > max) {
+                    sum = 2 * (k - min);
+                } else {
+                    sum = 2 * (max - min);
+                }
+                if (sum < bestSum) bestSum = sum;
+            }
+            System.out.println(bestSum);
+        }
+    }
+}
